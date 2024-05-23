394af138cce729a937e1e580bf57dcda45f6e80e
KyojuroB <129997667+KyojuroB@users.noreply.github.com>
2024-05-23 11:48:29 -0700

    18

--- Scuffed Robbery/Assets/Scripts/BankDelivery.cs
+++ Scuffed Robbery/Assets/Scripts/BankDelivery.cs
@@ -1,18 +1,91 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
 
 public class BankDelivery : MonoBehaviour
 {
-    // Start is called before the first frame update
+ 
+    [SerializeField] Material material;
+    [SerializeField] float maxRange;
+    public Material[] mat;
+    public List<Material> newmat;
+    MeshRenderer meshR;
+    bool mouseOver;
+   
+    bool isFinished = false;
+  public  bool isOpen = false;
+    Animator anim;
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        meshR =gameObject.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>();
+        mat = meshR.materials;
+        newmat = mat.ToList();
+        newmat.Add(material);
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+
+
+
+        if (mouseOver == true && Input.GetKeyDown(KeyCode.E))
+        {
+            if (!isOpen)
+            {
+                anim.SetBool("OpenDoor", true);
+                isOpen = true;
+               
+
+            }
+            else
+            {
+                anim.SetBool("OpenDoor", false);
+                isOpen = false;
+               
+            }
+             
+            Debug.Log("w");
+        }
+
+       
+        if (mouseOver && Vector3.Distance(transform.position, FindObjectOfType<PlayerMovement>().transform.position) < maxRange && !isFinished)
+        {
+            mouseOver = true;
+            meshR.materials = newmat.ToArray();
+        }
+        else
+        {
+            // Revert to original material 
+            mouseOver = false;
+            meshR.materials = mat;
+
+
+        }
+    }
+
+
+
+    private void OnMouseOver()
+    {
+
+
+        if (Vector3.Distance(transform.position, FindObjectOfType<PlayerMovement>().transform.position) < maxRange && !isFinished)
+        {
+            mouseOver = true;
+            meshR.materials = newmat.ToArray();
+        }
+    }
+
+    private void OnMouseExit()
+    {
+
+        mouseOver = false;
+        meshR.materials = mat;
+
     }
 }
